63e4c3b35ea53bafef3e1a5d2705c8c174d28fa5
Arndt Boris Fahr <git@abfx.de>
2015-07-02 14:57:57 +0200

    +Calendar export zu ics-Dateien (UID zu efaId)

--- src/de/nmichael/efa/calendar/ICalendarExport.java
+++ src/de/nmichael/efa/calendar/ICalendarExport.java
@@ -11,12 +11,10 @@ import net.fortuna.ical4j.data.CalendarOutputter;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Date;
 import net.fortuna.ical4j.model.DateTime;
-import net.fortuna.ical4j.model.Property;
 import net.fortuna.ical4j.model.Recur;
 import net.fortuna.ical4j.model.ValidationException;
 import net.fortuna.ical4j.model.WeekDay;
 import net.fortuna.ical4j.model.component.VEvent;
-import net.fortuna.ical4j.model.parameter.Value;
 import net.fortuna.ical4j.model.property.CalScale;
 import net.fortuna.ical4j.model.property.Description;
 import net.fortuna.ical4j.model.property.Location;
@@ -25,7 +23,6 @@ import net.fortuna.ical4j.model.property.RRule;
 import net.fortuna.ical4j.model.property.Uid;
 import net.fortuna.ical4j.model.property.Version;
 import net.fortuna.ical4j.model.property.XProperty;
-import net.fortuna.ical4j.util.UidGenerator;
 import de.nmichael.efa.Daten;
 import de.nmichael.efa.data.BoatReservationRecord;
 import de.nmichael.efa.data.BoatReservations;
@@ -39,8 +36,10 @@ import de.nmichael.efa.ex.EfaException;
 
 public class ICalendarExport {
 
-  public static final String CRLF = " " + net.fortuna.ical4j.util.Strings.LINE_SEPARATOR; // "\r\n"
+  public static final String CRLF = net.fortuna.ical4j.util.Strings.LINE_SEPARATOR; // "\r\n"
   private static final UUID BOOTSHAUS = new UUID(-7033734156567033637L, -8676639372818108974L);
+  private static final String EFA = "efa";
+  private static final String ABFX_DE = "@abfx.de";
 
   public void saveAllReservationToCalendarFile() throws IOException, ValidationException,
   ParserException, EfaException, ParseException {
@@ -48,10 +47,6 @@ public class ICalendarExport {
     // Creating a new calendar
     net.fortuna.ical4j.model.Calendar alleCalendar = new net.fortuna.ical4j.model.Calendar();
     net.fortuna.ical4j.model.Calendar bootshausCalendar = new net.fortuna.ical4j.model.Calendar();
-
-    Date bisSilvester = new Date();
-    bisSilvester.setMonth(12 - 1);
-    bisSilvester.setDate(31);
 
     BoatReservations boatReservations = Daten.project.getBoatReservations(false);
     for (DataKey<?, ?, ?> dataKey : boatReservations.data().getAllKeys()) {
@@ -73,7 +68,10 @@ public class ICalendarExport {
       String reason = boatReservationRecord.getReason();
       String reservationTimeDescription = boatReservationRecord.getReservationTimeDescription();
       DateTime dateTimeLastModified = new DateTime(boatReservationRecord.getLastModified());
+      String dateTimeLastModifiedStr = dateTimeLastModified.toString().replace('T', '.');
       int reservationOrder = boatReservationRecord.getReservation();
+      String efaId = EFA + reservationOrder + personAsName.substring(0, 1).toUpperCase();
+      String uid = dateTimeLastModifiedStr + "." + efaId + ABFX_DE;
 
       String descriptionAlle = reservationTimeDescription + CRLF;
       if (!"k.A.".equals(reason)) {
@@ -83,13 +81,13 @@ public class ICalendarExport {
       if (!"".equals(contactPhone)) {
         descriptionAlle += "Fon: " + contactPhone + CRLF;
       }
-      String modif = "(#" + reservationOrder + " aktualisiert am " + dateTimeLastModified + ")";
+      String modif = "(" + efaId + " aktualisiert am " + dateTimeLastModifiedStr + ")";
       descriptionAlle += modif;
       String descriptionBhnutzung = reservationTimeDescription + CRLF + modif;
 
       if (BoatReservationRecord.TYPE_WEEKLY.equals(type)) {
         dateFrom = DataTypeDate.today();
-        dateFrom.addDays(-1);
+        dateFrom.addDays(-7); // TODO next THursday
         dateTo = dateFrom;
       }
       DateTime startDateTime = new DateTime(dateFrom.getTimestamp(timeFrom));
@@ -97,22 +95,22 @@ public class ICalendarExport {
 
       // Creating an event
       VEvent buchungAlle = new VEvent(startDateTime, endDateTime, boatName + " - " + personAsName);
-      VEvent nurBootshaus = new VEvent(startDateTime, endDateTime,
-          boatName + " - #" + reservationOrder + personAsName.substring(0, 1));
+      VEvent nurBootshaus = new VEvent(startDateTime, endDateTime, boatName + " - " + efaId);
 
       if (BoatReservationRecord.TYPE_WEEKLY.equals(type)) {
-        Recur recur = new Recur(Recur.WEEKLY, bisSilvester);
+        // String recur3 = "RRULE:FREQ=" + type + ";BYDAY=" + dayOfWeek.substring(0, 2);
+        Recur recur = new Recur();
+        recur.setFrequency(Recur.WEEKLY);
         recur.getDayList().add(new WeekDay(dayOfWeek.substring(0, 2)));
-        RRule rRule = new RRule(recur);
-        buchungAlle.getProperties().add(rRule);
-        nurBootshaus.getProperties().add(rRule);
+        buchungAlle.getProperties().add(new RRule(recur));
+        nurBootshaus.getProperties().add(new RRule(recur));
       }
       buchungAlle.getProperties().add(new Description(descriptionAlle));
       buchungAlle.getProperties().add(new Location("Isekai 10 Hamburg"));
-      buchungAlle.getProperties().add(new UidGenerator("oh-id-" + reservationOrder).generateUid()); // TODO
+      buchungAlle.getProperties().add(new Uid(uid));
       nurBootshaus.getProperties().add(new Description(descriptionBhnutzung));
       nurBootshaus.getProperties().add(new Location("Isekai 10 Hamburg"));
-      nurBootshaus.getProperties().add(new UidGenerator("oh-id-" + reservationOrder).generateUid());
+      nurBootshaus.getProperties().add(new Uid(uid));
 
       alleCalendar.getComponents().add(buchungAlle);
       if (boatId.equals(BOOTSHAUS)) {
@@ -139,21 +137,22 @@ public class ICalendarExport {
       DataTypeDate workDate = clubworkRecord.getWorkDate();
       double hours = clubworkRecord.getHours();
       DateTime dateTimeLastModified = new DateTime(clubworkRecord.getLastModified());
-      UUID uuid = clubworkRecord.getId();
+      String dateTimeLastModifiedStr = dateTimeLastModified.toString().replace('T', '.');
+      String efaId = EFA + firstLastName.substring(0, 1).toUpperCase();
+      String uid = dateTimeLastModifiedStr + "." + efaId + ABFX_DE;
 
       String descriptionAlle = firstLastName + CRLF;
       descriptionAlle += description + CRLF;
       descriptionAlle += workDate + " ganze " + hours + " Stunden" + CRLF;
-      descriptionAlle += "(eingetragen am " + dateTimeLastModified + ")" + CRLF;
+      descriptionAlle += "(eingetragen am " + dateTimeLastModifiedStr + ")" + CRLF;
       if (clubworkRecord.getDeleted()) {
         descriptionAlle += "Dieser Eintrag wurde gelöscht" + CRLF;
       }
 
       VEvent event = new VEvent(new Date(workDate.getTimestamp(null)),
           firstLastName + " " + hours + "h " + description);
-      event.getProperties().getProperty(Property.DTSTART).getParameters().add(Value.DATE);
       event.getProperties().add(new Description(descriptionAlle));
-      event.getProperties().add(new Uid(uuid.toString()));
+      event.getProperties().add(new Uid(uid));
 
       calendar.getComponents().add(event);
     }
@@ -167,7 +166,7 @@ public class ICalendarExport {
     String path = Daten.efaBaseConfig.efaUserDirectory + Daten.fileSep + "backup" + Daten.fileSep;
     String dateTimeStamp = new DateTime().toString().replace('T', '.');
     String extension = ".ics";
-    String description = "Beschreibung " + title + " vom " + dateTimeStamp;
+    String description = title + " vom " + dateTimeStamp;
 
     iCalendar.getProperties().add(new ProdId("-//Arndt Boris Fahr//iCal4j 1.0//EN"));
     iCalendar.getProperties().add(Version.VERSION_2_0);
@@ -176,11 +175,9 @@ public class ICalendarExport {
     iCalendar.getProperties().add(new XProperty("X-WR-TIMEZONE", "Europe/Berlin"));
     iCalendar.getProperties().add(new XProperty("X-LIC-LOCATION", "Europe/Berlin"));
     iCalendar.getProperties().add(new XProperty("X-WR-CALDESC", description));
-    // iCalendar.getComponents().add(tz);
 
     // Saving as iCalendar file
     CalendarOutputter outputter = new CalendarOutputter();
-    // outputter.setValidating(false);
     outputter.setValidating(true);
     FileOutputStream foutohne = new FileOutputStream(path + title + extension);
     FileOutputStream foutdat = new FileOutputStream(path + dateTimeStamp + "." + title + extension);

490550018b66538952b194faf4328672ba1ccbf6
Boris Fahr <git@abfx.de>
2020-10-26 17:05:34 +0100

    ~jeder darf Nachricht an Admin schreiben - nicht nur Mitglieder

--- src/de/nmichael/efa/Daten.java
+++ src/de/nmichael/efa/Daten.java
@@ -67,8 +67,8 @@ public class Daten {
 
   // VersionsID: Format: "X.Y.Z_MM";
   // final-Version z.B. 1.4.0_00; beta-Version z.B. 1.4.0_#1
-  public final static String VERSIONID = "2.2.0_112";
-  public final static String VERSIONRELEASEDATE = "25.10.2020"; // Release Date: TT.MM.JJJJ
+  public final static String VERSIONID = "2.2.0_113";
+  public final static String VERSIONRELEASEDATE = "26.10.2020"; // Release Date: TT.MM.JJJJ
   public final static String MAJORVERSION = "2";
   public final static String PROGRAMMID = "EFA.220"; // Versions-ID für Wettbewerbsmeldungen
   public final static String PROGRAMMID_DRV = "EFADRV.220"; // Versions-ID für Wettbewerbsmeldungen
